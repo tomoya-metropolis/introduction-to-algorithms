6e1452abd46f4c67968e311a43aa7da945bfd71d
Tomoya Sasaki <tomoya.let.it.bleed@gmail.com>
2025-09-22 20:48:52 +0900

    Implement counting sort

---
+++ sorting_in_linear_time/counting_sort.py
@@ -0,0 +1,37 @@
+import random
+
+
+def sort(data):
+    counting_table = [0 for _ in range(max(data) + 1)]
+
+    for d in data:
+        counting_table[d] += 1
+
+    print(counting_table)
+
+    for i in range(len(counting_table) - 1):
+        counting_table[i + 1] += counting_table[i]
+
+    print(counting_table)
+
+    temp = [-1 for _ in range(len(data))]
+    for i in reversed(range(len(data))):
+        idx_to_insert = counting_table[data[i]] - 1
+        temp[idx_to_insert] = data[i]
+        counting_table[data[i]] -= 1
+
+    data.clear()
+    data.extend(temp)
+
+
+if __name__ == '__main__':
+    data = [random.randint(1, 20) for _ in range(40)]
+
+    print("--- before ---")
+    print(data)
+
+    print("--- sort ---")
+    sort(data)
+
+    print("--- after ---")
+    print(data)
